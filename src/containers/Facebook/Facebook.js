0bcecfd35100186f1f10905a307f7976d70874d7
priyankam <priyanka.mahajan@ispace.com>
2021-07-02 11:40:23 +0530

    added facebook structure

---
+++ src/containers/Facebook/Facebook.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from "./Facebook.css"
+
+const Facebook =props=> <div> hello</div>
+
+
+export default Facebook
---
+++ src/containers/Instagram/Instagram.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from "./Facebook.css"
+
+const Instagram =props=>{
+return <div></div>
+}
+
+export default Instagram;
